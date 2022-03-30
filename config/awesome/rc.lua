a99bc5bdcab1f06ec126c8161b106d6c9acb5538
yorkox <49209840+yorkox0@users.noreply.github.com>
2022-03-30 16:31:42 +0200

    Create rc.lua

---
+++ config/awesome/rc.lua
@@ -0,0 +1,52 @@
+pcall(require, "luarocks.loader")
+--[[
+ _____ __ _ __ _____ _____ _____ _______ _____
+|     |  | |  |  ___|  ___|     |       |  ___|
+|  -  |  | |  |  ___|___  |  |  |  | |  |  ___|
+|__|__|_______|_____|_____|_____|__|_|__|_____|
+               ~ AestheticArch ~
+                     rxyhn
+--]]
+
+
+-- Standard awesome library
+local gfs = require("gears.filesystem")
+local awful = require("awful")
+
+-- Theme handling library
+local beautiful = require("beautiful")
+theme_dir = gfs.get_configuration_dir() .. "theme/"
+dpi = beautiful.xresources.apply_dpi
+beautiful.init(gfs.get_configuration_dir() .. "theme/theme.lua")
+
+-- Default Applications
+terminal = "alacritty"
+editor = terminal .. " -e " .. os.getenv("EDITOR")
+vscode = "code"
+browser = "firefox"
+launcher = "rofi -show drun -theme " .. theme_dir .. "rofi.rasi"
+file_manager = "nautilus"
+music_client = terminal .. " --class music -e ncmpcpp"
+
+-- Weather API
+openweathermap_key = "" -- API Key
+openweathermap_city_id = "" -- City ID
+weather_units = "metric" -- Unit
+
+-- Global Vars
+screen_width = awful.screen.focused().geometry.width
+screen_height = awful.screen.focused().geometry.height
+
+-- Autostart
+local autostart = require("configuration.autostart")
+
+-- Import Configuration
+require("configuration")
+
+-- Import Daemons and Widgets
+require("signal")
+require("ui")
+
+-- Garbage Collector Settings
+collectgarbage("setpause", 110)
+collectgarbage("setstepmul", 1000)
