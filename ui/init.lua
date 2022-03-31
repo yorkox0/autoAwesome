ec0bb230086a86d00b28b213723637857b56e18b
yorkox <49209840+yorkox0@users.noreply.github.com>
2022-03-31 21:27:25 +0200

    Delete ui directory

--- ui/init.lua
+++
@@ -1,8 +0,0 @@
-local lock_screen = require("ui.lockscreen")
-lock_screen.init()
-
-require("ui.bar")
-require("ui.dashboard")
-require("ui.decorations")
-require("ui.notifs")
-require("ui.tooltip")
